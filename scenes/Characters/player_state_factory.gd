4c1bdc0df7c704f012578d08e32e92e6065d38f2
Tro0021 <abhirambellamkonda80@gmail.com>
2026-07-30 22:39:38 +0530

    added new recovery state

---
+++ scenes/Characters/player_state_factory.gd
@@ -0,0 +1,15 @@
+class_name PlayerStateFactory
+
+var states : Dictionary
+
+func _init() -> void:
+	states = {
+		Player.State.MOVING: PlayerStateMoving,
+		Player.State.TACKLING: PlayerStateTackling,
+		Player.State.RECOVERING: PlayerStateRecovering,
+		
+	}
+
+func get_fresh_state(state: Player.State) -> PlayerState:
+	assert(states.has(state), "state doesn'y exist")
+	return states.get(state).new()
