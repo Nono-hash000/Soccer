ccae1fc7e306aa1c9be0659412ea213609a1df41
Tro0021 <abhirambellamkonda80@gmail.com>
2026-07-30 22:02:18 +0530

    added tackle animation

---
+++ scenes/Characters/player.gd
@@ -0,0 +1,47 @@
+class_name Player
+extends CharacterBody2D
+
+const DURATION_TACKLE := 200
+
+enum ControlScheme{CPU, P1, P2}
+enum State {MOVING, TACKLING}
+
+@export var control_scheme : ControlScheme
+@export var speed : float
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var player_sprite: Sprite2D = $PlayerSprite
+
+var heading := Vector2.RIGHT
+
+func _process(_delta: float) -> void:
+	if control_scheme == ControlScheme.CPU:
+		pass
+	else:
+		handle_human_movement()
+	set_movement_animation()
+	set_heading()
+	flip_spriteS()
+	move_and_slide()
+
+func handle_human_movement() -> void:
+	var direction := KeyUtils.get_input_vector(control_scheme)
+	velocity = direction * speed
+
+func set_movement_animation() -> void:
+	if velocity.length() > 0:
+		animation_player.play("run")
+	else:
+		animation_player.play("idle")
+
+func set_heading() -> void:
+	if velocity.x > 0:
+		heading = Vector2.RIGHT
+	elif velocity.x < 0:
+		heading = Vector2.LEFT
+
+func flip_spriteS() -> void:
+	if heading == Vector2.RIGHT:
+		player_sprite.flip_h = false
+	elif heading == Vector2.LEFT:
+		player_sprite.flip_h = true
