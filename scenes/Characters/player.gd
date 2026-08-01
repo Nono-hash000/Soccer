874db4707e7cc686c2c14174503ae131411198ca
Tro0021 <abhirambellamkonda80@gmail.com>
2026-08-01 20:59:43 +0530

    added passing state

--- scenes/Characters/player.gd
+++ scenes/Characters/player.gd
@@ -2,13 +2,14 @@ class_name Player
 extends CharacterBody2D
 
 enum ControlScheme{CPU, P1, P2}
-enum State {MOVING, TACKLING, RECOVERING, PREPPING_SHOT, SHOOTING}
+enum State {MOVING, TACKLING, RECOVERING, PREPPING_SHOT, SHOOTING, PASSING}
 
 @export var control_scheme : ControlScheme
 @export var speed : float
 @export var power : float
 @export var ball : Ball
 
+@onready var teammate_detection_area: Area2D = $TeammateDetectionArea
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var player_sprite: Sprite2D = $PlayerSprite
 
@@ -27,7 +28,7 @@ func switch_state(state: State, state_data: PlayerStateData = PlayerStateData.ne
 	if current_state != null:
 		current_state.queue_free()
 	current_state = state_factory.get_fresh_state(state)
-	current_state.setup(self, state_data, animation_player, ball)
+	current_state.setup(self, state_data, animation_player, ball, teammate_detection_area)
 	current_state.state_transition_requested.connect(switch_state.bind())
 	current_state.name = "PlayerStateMachine: " + str(state)
 	call_deferred("add_child", current_state)
