ccdf23245d872277428bc6295ae9cd7f9f5367f5
Tro0021 <abhirambellamkonda80@gmail.com>
2026-08-01 22:05:01 +0530

    added shadow and control indicator

--- scenes/ball/ball.gd
+++ scenes/ball/ball.gd
@@ -3,6 +3,9 @@ extends AnimatableBody2D
 
 enum State {CARRIED, FREEFORM, SHOT}
 
+@export var friction_air : float
+@export var friction_ground : float
+
 @onready var player_detection_area: Area2D = $PlayerDetectionArea
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var ball_sprite: Sprite2D = $BallSprite
@@ -33,3 +36,11 @@ func shoot(shot_velocity : Vector2)-> void:
 	velocity = shot_velocity
 	carrier = null
 	switch_states(Ball.State.SHOT)
+
+func pass_to(destination: Vector2) -> void:
+	var direction := position.direction_to(destination)
+	var distance := position.distance_to(destination)
+	var intensity := sqrt(2 * distance * friction_ground)
+	velocity = intensity * direction
+	carrier = null
+	switch_states(Ball.State.FREEFORM)
--- scenes/ball/ball_states/ball_state_shot.gd
+++ scenes/ball/ball_states/ball_state_shot.gd
@@ -2,7 +2,7 @@ class_name BallStateShot
 extends BallState
 
 const SHOT_SPRITE_SCALE := 0.8
-const SHOT_HEIGHT := 30
+const SHOT_HEIGHT := 5
 const DURATION_SHOT := 1000
 
 var time_since_shot := Time.get_ticks_msec()
